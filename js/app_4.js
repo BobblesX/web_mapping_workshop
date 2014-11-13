fb46b142a45e50c19b9ec282c4535edfc9828343
BobblesX <BobblesX@users.noreply.github.com>
2014-11-13 15:12:47 -0600

    Update app_4.js

--- js/app_4.js
+++ js/app_4.js
@@ -1,8 +1,16 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
-
 var mapId = 'bobblesx.k7be10ag'; //<- this references the ugly green map that I made
 var token = 'pk.eyJ1IjoiYm9iYmxlc3giLCJhIjoibHZGdFNRVSJ9.u1_VcdT6AdCBdeBYm4mmTA'; //<- this is my token, use yours.
+//Create the map object with your mapId and token
+L.mapbox.accessToken = token;
+var map = L.mapbox.map('map', mapId);
+//Set the view of the map to the whole US
+map.setView([38.84291652482239, -105.04165649414062], 7);
+///////////////////////////////////////////////////////////////////////////
+// This is the area we're going to use to add data to our map
+var dataFileToAdd = 'data/pikes_peak.geojson'; //<- Point this to the file that you want to include on the map
+var dataToAdd;
 
 var featureLayer = L.mapbox.featureLayer()
     .loadURL(dataFileToAdd)
