659ef8cf889c92b58c40046adf02c372e7d36848
BobblesX <BobblesX@users.noreply.github.com>
2014-11-13 15:09:06 -0600

    Update app_4.js

--- js/app_4.js
+++ js/app_4.js
@@ -2,6 +2,7 @@
 // Enter your mapbox map id here to reference it for the base layer
 
 var mapId = 'bobblesx.k7be10ag'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiYm9iYmxlc3giLCJhIjoibHZGdFNRVSJ9.u1_VcdT6AdCBdeBYm4mmTA'; //<- this is my token, use yours.
 
 var featureLayer = L.mapbox.featureLayer()
     .loadURL(dataFileToAdd)
